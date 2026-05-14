1016a9d3e819f1b2aa957ef05fd2992027e2e25f
Maik G <maik.g@outlook.com>
2026-05-14 10:45:52 +0200

    Add Django Filters to OfferListView for enhanced querying capabilities

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'rest_framework',
+    'django_filters',
     'auth_app',
     'profile_app',
     'offers_app',
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,4 +1,5 @@
 
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -7,35 +8,51 @@ from offers_app.models import OfferModel, DetailModel
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsBusinessUserOrAdmin
 from .pagination import LargeResultsSetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
-class OfferListView(APIView):
 
+class OfferListView(generics.ListCreateAPIView):
+
+    queryset = OfferModel.objects.all()
     permission_classes = [IsAuthenticated, IsBusinessUserOrAdmin]
     pagination_class = LargeResultsSetPagination
-  
-    def post(self, request):
-        try:
-            serializer = OfferPostSerializer(data=request.data)
-            self.check_permissions(request, serializer)
-            if serializer.is_valid():
-                saved_offer = serializer.save(user=request.user)
-                return Response(OfferPostSerializer(saved_offer).data, status=201)
-            else:
-                return Response(serializer.errors, status=400)
-        except: 
-         return Response({'error':'Authentifizierter Benutzer ist kein `business` Profil'}, status=403)
-        
-    def get(self, request):
-        try:
-            offer = OfferModel.objects.all()
-            paginator = self.pagination_class()
-            result_page = paginator.paginate_queryset(offer, request)
-            serializer = OfferGetSerializer(result_page, many=True)
-            return paginator.get_paginated_response(serializer.data)
-        except OfferModel.DoesNotExist:
-            return Response ({"error" : "Das Angebot mit der angegebenen ID wurde nicht gefunden."}, status=404)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    
+    filterset_fields = ['creator_id', 'min_price', 'max_delivery_time']
+    search_fields = ['title', 'description']
+    ordering_fields = ['updated_at', 'min_price']
+    ordering = ['updated_at', 'min_price' ]
 
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OfferPostSerializer
+        return OfferGetSerializer
+  
+    # def post(self, request):
+    #     try:
+    #         serializer = OfferPostSerializer(data=request.data)
+    #         self.check_permissions(request, serializer)
+    #         if serializer.is_valid():
+    #             saved_offer = serializer.save(user=request.user)
+    #             return Response(OfferPostSerializer(saved_offer).data, status=201)
+    #         else:
+    #             return Response(serializer.errors, status=400)
+    #     except: 
+    #      return Response({'error':'Authentifizierter Benutzer ist kein `business` Profil'}, status=403)
+        
+    # def get(self, request):
+    #     try:
+    #         offer = OfferModel.objects.all()
+    #         paginator = self.pagination_class()
+    #         result_page = paginator.paginate_queryset(offer, request)
+    #         serializer = OfferGetSerializer(result_page, many=True)
+    #         return paginator.get_paginated_response(serializer.data)
+    #     except OfferModel.DoesNotExist:
+    #         return Response ({"error" : "Das Angebot mit der angegebenen ID wurde nicht gefunden."}, status=404)
+        
 class OfferDetailView(APIView):
 
     permission_classes = [IsAuthenticated, IsBusinessUserOrAdmin]
