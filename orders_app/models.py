e0727651b4365a70137feb2cd6e8c510d91c7892
Maik G <maik.g@outlook.com>
2026-05-16 18:33:57 +0200

    Add OffersModel and initial migration for orders app

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class OffersModel(models.Model):
+    customer_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders_as_customer')
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders_as_business')
+    title = models.CharField(max_length=100)
+    revisions = models.IntegerField()
+    delivery_time_in_days = models.IntegerField()
+    price = models.IntegerField()
+    features = models.JSONField()
+    offer_type = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
