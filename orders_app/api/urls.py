4cd9cdc303287b9a552d815417ff7f699b3e6258
Maik G <maik.g@outlook.com>
2026-05-17 18:33:22 +0200

    Add: Implement OrderDetailView for retrieving order details

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderListView
+from .views import OrderListView, OrderDetailView
 
 urlpatterns = [
      path('orders/', OrderListView.as_view(), name='orders'),
+     path('orders/<int:pk>/', OrderDetailView.as_view(), name='orders_detail'),
 ]
--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from orders_app.models import OrdersModel
 from .serializers import OrdersSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 
 class OrderListView(generics.ListCreateAPIView):
@@ -14,3 +17,17 @@ class OrderListView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(customer_user=self.request.user)
+
+
+class OrderDetailView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+
+        try:
+            order = OrdersModel.objects.get(pk=pk)
+            serializer = OrdersSerializer(order)
+            return Response (serializer.data)
+        except OrdersModel.DoesNotExist:
+            return Response ({"error" : "Das Angebot mit der angegebenen ID wurde nicht gefunden."}, status=404)
