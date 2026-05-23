ff4cdd1d8c18e64720a1a7375be327ed9d7d0d64
croser93 <maik.g@outlook.com>
2026-05-23 17:27:14 +0200

    Add: Tests in orders_app

---
+++ orders_app/tests/test_orders.py
@@ -0,0 +1,46 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+import copy
+
+from offers_app.models import OfferModel, DetailModel
+from reviews_app.models import ReviewModel
+from auth_app.models import UserProfile
+from orders_app.models import OrdersModel
+
+
+class OrdersTest(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='123456', email='test@test.de')
+        self.business_user = User.objects.create_user(username='business_user', password='123456', email='business@test.de')
+
+
+        self.offer = OfferModel.objects.create(title='test', description='dies ist eine test beschreibung', user=self.business_user,)
+        self.detail = DetailModel.objects.create(offer=self.offer, title='test', revisions= 2 ,delivery_time_in_days= 5, price=300, features=['superduper',"test"], offer_type='basic' )
+        self.order = OrdersModel.objects.create(
+            customer_user=self.user, 
+            business_user=self.business_user,
+            title='Test Titel',
+            revisions=5,
+            delivery_time_in_days=5,
+            price=1000,
+            features=['style', 'test'],
+            offer_type='basic',
+            status='in_progress'
+            )
+        
+        self.userprofile_costumer = UserProfile.objects.create(user=self.user, type='customer')
+        self.userprofile_business = UserProfile.objects.create(user=self.business_user, type='business')
+
+        self.token = Token.objects.create(user=self.user)
+        self.token_business = Token.objects.create(user=self.business_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+
+    def  test_get_order(self):
+        url= reverse('orders')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
